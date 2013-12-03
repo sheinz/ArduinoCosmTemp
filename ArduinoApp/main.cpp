e5041f05776e2b2d59c0faef9e719f3914937dc3
Yuriy Skriyka <yuraofo@gmail.com>
2013-12-03 23:25:03 +0200

    Placed string into program memory. Switched to the new xively feed

--- ArduinoApp/main.cpp
+++ ArduinoApp/main.cpp
@@ -10,11 +10,17 @@
 
 #include "xively.key"
 
+// Workaround for http://gcc.gnu.org/bugzilla/show_bug.cgi?id=34734
+#ifdef PROGMEM
+#undef PROGMEM
+#define PROGMEM __attribute__((section(".progmem.data")))
+#endif
+
 // MAC address for your Ethernet shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 // Your Xively key to let you upload data
-char cosmKey[] = PRIVATE_XIVELY_KEY;  // defined in cosm.key
+const char* cosmKey = PRIVATE_XIVELY_KEY;  // defined in cosm.key
 
 
 // Define the strings for our datastream IDs
@@ -30,7 +36,7 @@ XivelyDatastream datastreams[] = {
    XivelyDatastream(pressureId, strlen(pressureId), DATASTREAM_FLOAT),
 };
 // Finally, wrap the datastreams into a feed
-XivelyFeed feed(102125, datastreams, 4 /* number of datastreams */);
+XivelyFeed feed(1226691453, datastreams, 4 /* number of datastreams */);
 
 EthernetClient client;
 XivelyClient xivelyclient(client);
@@ -48,25 +54,25 @@ void setup()
    // put your setup code here, to run once:
    Serial.begin(9600);
 
-   Serial.println("Starting multiple datastream upload to Xively...");
+   Serial.println(F("Starting multiple datastream upload to Xively..."));
    Serial.println();
 
    while (Ethernet.begin(mac) != 1)
    {
-      Serial.println("Error getting IP address via DHCP, trying again...");
+      Serial.println(F("Error getting IP address via DHCP, trying again..."));
       delay(15000);
    }
 
    numberOfSensors = ds.search();
 
-   Serial.print("Found sensors: ");
+   Serial.print(F("Found sensors: "));
    Serial.println(numberOfSensors);
 
    dht.begin();
 
    if (!bmp.begin())
    {
-      Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+      Serial.println(F("Could not find a valid BMP085 sensor, check wiring!"));
       while (1) {}
    }
 }
@@ -75,7 +81,7 @@ void loop()
 {
    if (numberOfSensors < 2)
    {
-      Serial.println("Not enough sensors. Need at least two sensors.");
+      Serial.println(F("Not enough sensors. Need at least two sensors."));
       numberOfSensors = ds.search();
       delay(3000);
       return;
@@ -90,38 +96,38 @@ void loop()
 
    if (indoorTemp < -100 || outdoorTemp < -100)
    {
-      Serial.println("Invalid temperature");
+      Serial.println(F("Invalid temperature"));
       delay(3000);
       return;
    }
 
    if (isnan(humidity))
    {
-      Serial.println("Failed to read from DHT");
+      Serial.println(F("Failed to read from DHT"));
       delay(3000);
       return;
    }
 
    datastreams[0].setFloat(indoorTemp);
-   Serial.print("Read indoor sensor value ");
+   Serial.print(F("Read indoor sensor value "));
    Serial.println(datastreams[0].getFloat());
 
    datastreams[1].setFloat(outdoorTemp);
-   Serial.print("Read outdoor sensor value ");
+   Serial.print(F("Read outdoor sensor value "));
    Serial.println(datastreams[1].getFloat());
 
    datastreams[2].setFloat(humidity);
-   Serial.print("Humidity: ");
+   Serial.print(F("Humidity: "));
    Serial.println(datastreams[2].getFloat());
 
    datastreams[3].setFloat(pressure);
-   Serial.print("Pressure = ");
+   Serial.print(F("Pressure = "));
    Serial.print(datastreams[3].getFloat());
    Serial.println(" Pa");
 
-   Serial.println("Uploading it to Xively");
+   Serial.println(F("Uploading it to Xively"));
    int ret = xivelyclient.put(feed, cosmKey);
-   Serial.print("cosmclient.put returned ");
+   Serial.print(F("xivelyclient.put returned "));
    Serial.println(ret);
 
    Serial.println();
--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Arduino COSM Temperature
 
 This is a simple Arduino project. It uses two digital temperature sensors (DS18B20) to collect indoor and outdoor temperature. Than it sends the data to the COSM (cosm.com - sensor data hosting).
 
-COSM url: https://cosm.com/feeds/102125
+COSM url: https://xively.com/feeds/1226691453
 
